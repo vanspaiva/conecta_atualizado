d647bcbddf0ae1d7f157c6f0fb58de503c364858
Samuel Oliveira <samuel.silva@fixhealth.com.br>
2024-07-23 08:54:54 -0300

    teste dbh

---
+++ includes/dbh.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+// $serverName = "bdcpmhconecta.mysql.dbaas.com.br";
+// $serverName = "localhost";
+// $dbUsername = "bdcpmhconecta";
+// $dbPassword = "YDT8HJPHc!";
+// $dbName = "bdcpmhconecta";
+
+$serverName = "localhost";
+$dbUsername = "u777557116_adminconecta";
+$dbPassword = "YDT8HJPHc!";
+$dbName = "u777557116_bdcpmhconecta";
+
+/* $serverName = "localhost:3307";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "conecta";
+ */
+$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
+
+if(mysqli_connect_errno())
+{
+    echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
