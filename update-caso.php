a63d441f937861db10e8910b589d8966f68993d3
Samuel Oliveira <samuel.silva@fixhealth.com.br>
2024-07-25 12:32:28 -0300

    feat: adicionado a possibilidade de alterar a data de aceite

--- update-caso.php
+++ update-caso.php
@@ -494,12 +494,62 @@ if (!empty($_GET) && isset($_SESSION["useruid"]) && (($_SESSION["userperm"] == '
                                                         <div class="col">
                                                             <hr>
                                                             <div class="form-row">
-                                                                <div class="form-group col-md p-2">
-                                                                    <div class="d-flex justify-content-center">
+                                                                <div class="form-group col-md p-2 d-flex align-items-center flex-row justify-content-center">
+                                                                    <div class="d-flex justify-content-center mx-5">
                                                                         <a href="reabriraceite?id=<?php echo $row['pedNumPedido']; ?>" class="btn btn-outline-conecta" type="">
                                                                             Reabrir Aceite
                                                                         </a>
                                                                     </div>
+
+
+                                                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Alterar data do aceite</button>
+
+                                                                    <!-- Modal -->
+                                                                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                                        <div class="modal-dialog">
+                                                                            <div class="modal-content">
+                                                                                <div class="modal-header">
+                                                                                    <h5 class="modal-title" id="exampleModalLabel">Alterar data do Aceite</h5>
+                                                                                </div>
+                                                                                <div class="modal-body">
+                                                                                    <form action="alterarDataAceite.php" method="get">
+                                                                                        <div class="mb-3">
+                                                                                            <label for="recipient-name" class="col-form-label">Informe a data:</label>
+                                                                                            <input type="date" class="form-control" id="recipient-data" name="dataAceite" required>
+                                                                                        </div>
+                                                                                        <div class="mb-3" hidden>
+                                                                                            <label for="recipient-name" class="col-form-label">Numero do Pedido:</label>
+                                                                                            <input type="text" class="form-control" id="recipient-num" name="numeroPedido"
+                                                                                            value="<?php echo $row['pedNumPedido']; ?>" required >
+                                                                                        </div>
+                                                                                        <div class="mb-3" hidden>
+                                                                                            <label for="recipient-name" class="col-form-label">ID do Pedido:</label>
+                                                                                            <input type="text" class="form-control" id="recipient-id" name="idPedido"
+                                                                                            value="<?php echo $row['pedId']; ?>" required >
+                                                                                        </div>
+                                                                                        <div class="modal-footer">
+                                                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                                                                                            <button type="submit" class="btn btn-primary">Enviar</button>
+                                                                                        </div>
+                                                                                    </form>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+    </div>
+
+
+                                                                    <script
+                                                                        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+                                                                        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+                                                                        crossorigin="anonymous"
+                                                                    ></script>
+
+                                                                    <script
+                                                                        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+                                                                        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+                                                                        crossorigin="anonymous"
+                                                                    ></script>
+
                                                                 </div>
                                                             </div>
                                                         </div>
