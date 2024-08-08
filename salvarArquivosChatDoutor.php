7072587868149366b54f7e2727e9ed61ec48613f
Samuel Oliveira <samuel.silva@fixhealth.com.br>
2024-08-08 11:22:54 -0300

    salvando alterações

--- salvarArquivosChatDoutor.php
+++ salvarArquivosChatDoutor.php
@@ -16,12 +16,8 @@ if (isset($_GET['link'])) {
     
     $idPedido = $_GET['idPedido'];
     $mediaUser = $_GET['mediaUser'];
-    if(isset($_GET['dataUpload'])){
-        $dataUpload = $_GET['dataUpload'];
-    }
-    else{
-        $dataUpload = "NAO ESTOU RECEBENDO NADA";
-    }
+    $dataUpload = $_GET['dataUpload'];
+    $dataUpload = "NAO ESTOU RECEBENDO NADA";
     
     $nomearquivo = $_GET['nomeArquivo'];
     $tipoUser = $_GET['tipoUser'];
